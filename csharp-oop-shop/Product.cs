65973d42873dd4a30d608fae18fe33b6d76d0c08
Andrea Bonaventura <andreabonaventura802@gmail.com>
2023-04-07 16:49:41 +0200

    Aggiungere i file di progetto.

---
+++ csharp-oop-shop/Product.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_oop_shop
+{
+    internal class Product
+    {
+        private int codice;
+        private string nome;
+        private string descrizione;
+        private decimal prezzo;
+        private int iva;
+
+        public int Codice { get; }
+        public string Nome { get; set; }
+        public string Descrizione { get; set; }
+        public decimal Prezzo { get; set; }
+        public int Iva { get; set; }
+
+        Random rand = new Random();
+
+        public decimal fullPrice(decimal Prezzo)
+        {
+            return Prezzo * 22 / 100;
+        }
+
+        public Product(string nome, string descrizione, decimal prezzo, int iva)
+        {
+            Codice = rand.Next(1,1000);
+            Nome = nome;
+            Descrizione = descrizione;
+            Prezzo = prezzo;
+            Iva = iva;
+
+        }
+
+    }
+}
---
+++ csharp-oop-shop/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace csharp_oop_shop
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Product prodotto1 = new Product("Prodotto1", "Descrizione1", 200, 22);
+
+            Console.WriteLine(prodotto1);
+        }
+    }
+}
